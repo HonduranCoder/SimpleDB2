3ac8e06feab9ef924580ac6a3186b96554c5b00c
Estefani <stef45689@gmail.com>
2022-02-14 16:43:31 -0600

    get(id) w/passing test

--- __tests__/simple-db.test.js
+++ __tests__/simple-db.test.js
@@ -1,19 +1,30 @@
 const fs = require('fs/promises');
 const path = require('path');
+const SimpleDb = require('../lib/simple-db.js');
 
+//check this later (CI)
 const { CI, HOME } = process.env;
 const BASE_DIR = CI ? HOME : __dirname;
 const TEST_DIR = path.join(BASE_DIR, 'test-dir');
 
 describe('simple database', () => {
-
   beforeEach(async () => {
     await fs.rm(TEST_DIR, { force: true, recursive: true });
     await fs.mkdir(TEST_DIR, { recursive: true });
   });
+  //^ setting things up but just different syntax
+  it('gets by id', async () => {
+    //every time we do a test
+    const firstDb = new SimpleDb(TEST_DIR);
+    const newFile = {
+      id: '1',
+      content: 'new file',
+    };
 
-  it('needs a first test...', async () => {
-
+    const filePath = path.join(TEST_DIR, `${newFile.id}.txt`);
+    //writing the file
+    await fs.writeFile(filePath, JSON.stringify(newFile));
+    //checking the get(id) returns the file, we want the value
+    expect(await firstDb.get(newFile.id)).toEqual(newFile);
   });
-
 });
