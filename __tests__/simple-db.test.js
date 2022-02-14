7313df6141daa81884e5adc2c29ef618e45dcd66
Estefani <stef45689@gmail.com>
2022-02-14 17:43:40 -0600

    Working w/all tests passing before refactor

--- __tests__/simple-db.test.js
+++ __tests__/simple-db.test.js
@@ -1,5 +1,6 @@
 const fs = require('fs/promises');
 const path = require('path');
+const shortid = require('shortid');
 const SimpleDb = require('../lib/simple-db.js');
 
 //check this later (CI)
@@ -26,5 +27,46 @@ describe('simple database', () => {
     await fs.writeFile(filePath, JSON.stringify(newFile));
     //checking the get(id) returns the file, we want the value
     expect(await firstDb.get(newFile.id)).toEqual(newFile);
+  });
+  it('gives a bad file error', async () => {
+    const firstDb = new SimpleDb(TEST_DIR);
+    const newFile = {
+      id: '1',
+      content: 'new file',
+    };
+    fs.writeFile(TEST_DIR + 1 + 'txt', JSON.stringify(newFile));
+    const result = await firstDb.get(1);
+    expect(result.message).toEqual('Not Found');
+  });
+  it('saves an object id, assigns an id, and serializes the object', async () => {
+    const firstDb = new SimpleDb(TEST_DIR);
+    const newObject = {
+      name: 'new',
+      text: 'random',
+    };
+    return await firstDb
+      .save(newObject)
+      .then(() => expect(newObject.id).toEqual(expect.any(String)));
+  });
+  it('should return all files', () => {
+    const simpleDB = new SimpleDb(TEST_DIR);
+    const firsNew = { text: 'first' };
+    const secondOld = { text: 'second' };
+    const expectation = [
+      {
+        id: expect.any(String),
+        text: expect.any(String),
+      },
+      {
+        id: expect.any(String),
+        text: expect.any(String),
+      },
+    ];
+
+    return simpleDB
+      .save(firsNew)
+      .then(() => simpleDB.save(secondOld))
+      .then(() => simpleDB.getAll())
+      .then((files) => expect(files).toEqual(expectation));
   });
 });
